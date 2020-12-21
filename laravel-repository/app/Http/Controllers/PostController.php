5388dd7d87b588dcf662f9ea708a762a879b2320
Thao Trinh Minh <thaotrinhminh@gmail.com>
2020-12-21 15:50:12 +0700

    update db

--- laravel-repository/app/Http/Controllers/PostController.php
+++ laravel-repository/app/Http/Controllers/PostController.php
@@ -7,14 +7,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
     public function index(){
 
     }
 
     public function showByCategory($cate_id){
         $posts = Post::where('category_id','=',$cate_id)->get();
-        //dd($posts[0]->category);
         return view('post.index', compact('posts'));
     }
 }
